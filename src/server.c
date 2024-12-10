e412634be4b111e427ab4b478bc72cec6048db63
Saif Al-Din Samy Hussein <SeifOwleN@proton.me>
2024-12-10 02:17:22 +0200

    some get request implementation

--- src/server.c
+++ src/server.c
@@ -1,6 +1,7 @@
 #include "../include/balls.h"
 #include <aio.h>
 #include <arpa/inet.h>
+#include <asm-generic/socket.h>
 #include <bits/types/struct_timeval.h>
 #include <fcntl.h>
 #include <netdb.h>
@@ -26,20 +27,101 @@ int set_nonblocking(int fd) {
   return fcntl(fd, F_SETFL, erm | O_NONBLOCK);
 }
 
-// Handling GET requests
-void handle_get(int client_fd) {
-  const char *response_body = "Hello, GET!";
-  size_t content_length = strlen(response_body);
-
-  char response[BUF_SIZE];
-  snprintf(response, BUF_SIZE,
+// formant response
+void response(char *response_buf, int code, const char *content_type) {
+  ssize_t content_length = strlen(response_buf);
+  char response_body[BUF_SIZE];
+  strcpy(response_body, response_buf);
+  snprintf(response_buf, BUF_SIZE,
            "HTTP/1.1 200 OK\r\n"
            "Content-Length: %zu\r\n"
-           "Content-Type: text/plain\r\n"
+           "Content-Type: %s\r\n"
+           "Server: CTTP\r\n"
            "\r\n"
            "%s",
-           content_length, response_body);
-  send(client_fd, response, strlen(response), 0);
+           content_length, content_type, response_body);
+}
+
+char *parse_file_type(const char *file) {
+  char *dot = strrchr(file, '.');
+  if (dot == NULL) {
+    return "text/plain";
+  }
+  char file_type[dot - file];
+  strncpy(file_type, dot + 1, dot - file);
+  file_type[dot - file] = '\0';
+  printf("file_type: %s\n", file_type);
+  if (strcmp(file_type, "html") == 0) {
+    return "text/html";
+  } else if (strcmp(file_type, "txt") == 0) {
+    return "text/plain";
+  }
+  return "text/bozo";
+};
+
+int set_socket_opts(int fd) {
+  // For Handling address in use even after killing server
+  // (SO_REUSEADDR)
+  int opt = 1;
+  if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+    perror("setsockopt failed");
+    return -1;
+  }
+
+  struct linger so_linger;
+  so_linger.l_onoff = 1;
+  so_linger.l_linger = 30;
+
+  if (setsockopt(fd, SOL_SOCKET, SO_LINGER, &so_linger, sizeof(so_linger)) ==
+      -1) {
+    perror("SERVER: setsockopt SO_LINGER failed");
+    return -1;
+  }
+  return 0;
+}
+
+// Handling GET requests
+void handle_get(int client_fd, const char *path) {
+  char response_buf[BUF_SIZE] = "Hello, GET!\0";
+  char *content_type = "text/plain\0";
+  int code = 200;
+
+  // if path specified
+  if (strcmp(path, "/") != 0) {
+    char path_good[strlen(path)];
+    strncpy(path_good, path + 1, strlen(path) - 1);
+    path_good[strlen(path) - 1] = '\0';
+    int fd = open(path_good, O_RDONLY, S_IRWXU);
+    if (fd == -1) {
+      perror("SERVER: error opening file");
+      printf("file name: %s\n", path_good);
+      code = 404;
+    }
+    int err = read(fd, response_buf, BUF_SIZE);
+    if (err == -1) {
+      perror("SERVER: error reading file");
+      code = 500;
+    }
+    content_type = parse_file_type(path_good);
+
+  } else {
+    // serve index.html
+    int fd = open("index.html", O_RDONLY, S_IRWXU);
+    if (fd == -1) {
+      perror("SERVER: error opening index.html");
+      code = 404;
+    }
+    int err = read(fd, response_buf, BUF_SIZE);
+    if (err == -1) {
+      perror("SERVER: error reading file");
+      code = 500;
+    }
+    content_type = parse_file_type("index.html");
+  }
+
+  response(response_buf, code, content_type);
+
+  send(client_fd, response_buf, strlen(response_buf), 0);
 };
 
 // Handling POST requests
@@ -102,7 +184,9 @@ void handle_req(int *client_fd) {
 
   // Handle The Request
   if (strncmp(buf, "GET", 3) == 0) {
-    handle_get(*client_fd);
+    char *path = strtok(buf, " ");
+    path = strtok(NULL, " ");
+    handle_get(*client_fd, path);
   } else if (strncmp(buf, "POST", 4) == 0) {
     handle_post(*client_fd);
   } else if (strncmp(buf, "PUT", 3) == 0) {
@@ -125,13 +209,7 @@ int main() {
   struct in_addr in_addr;
   inet_pton(AF_INET, "127.0.0.1", &in_addr);
 
-  // For Handling address in use even after killing server
-  // (SO_REUSEADDR)
-  int opt = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) ==
-      -1) {
-    perror("setsockopt failed");
-  }
+  set_socket_opts(server_fd);
 
   struct sockaddr_in *sockaddr = malloc(sizeof(struct sockaddr_in));
   memset(sockaddr, 0, sizeof(struct sockaddr_in));
@@ -140,8 +218,7 @@ int main() {
   sockaddr->sin_port = htons(6969);
 
   // binding socket with address
-  if (bind(server_fd, (const struct sockaddr *)sockaddr,
-           sizeof(struct sockaddr_in)) == -1) {
+  if (bind(server_fd, (struct sockaddr *)sockaddr, sizeof(*sockaddr)) == -1) {
     perror("bind failed");
     close(server_fd);
     return 1;
